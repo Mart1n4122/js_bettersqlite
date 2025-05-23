2c4834b8e731996e1a6e037bae264618b5a285c6
Tóthmihály Martin <b1totmar@vasvari.org>
2025-05-23 14:28:24 +0200

    ..

--- jsbettersql/util/database.js
+++ jsbettersql/util/database.js
@@ -1,10 +1,6 @@
-import express from 'express';
 import Database from 'better-sqlite3';
+const db = new Database('./data/notes.sqlite');
 
-const app = express();
-const db = new Database('./data/notes.db');
-
-app.use(express.json());
 
 db.exec(`
   CREATE TABLE IF NOT EXISTS notes (
